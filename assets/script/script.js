574e17fe4bfcecdfcb30f04b818828d194da8d1c
ElmerPC <erey2790@gmail.com>
2021-12-21 21:10:58 -0600

    added second repo

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,6 @@
+//REPO FOR COCKTAIL API
 var getUserRepos = function() {
-    fetch("https://the-cocktail-db.p.rapidapi.com/filter.php", {
+    fetch("https://the-cocktail-db.p.rapidapi.com/filter.php?i=", {
     	"method": "GET",
     	"headers": {
     		"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
@@ -7,9 +8,32 @@ var getUserRepos = function() {
     	}
     })
     .then(response => {
-    	console.log(response);
+    	return response.json()
     })
+
+	.then(response =>{
+		console.log(response);
+	})
     .catch(err => {
     	console.error(err);
     });
 };
+getUserRepos()
+
+//REPO FOR SECOND API (CURRENTLY NOT WORKING) FINDING OTHER ALTERNATIVES
+var getUserRepos2 = function() {
+	fetch("https://walmart.p.rapidapi.com/v2/auto-complete?term=macbook%20air", {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "walmart.p.rapidapi.com",
+			"x-rapidapi-key": "2df09f326cmshd68e7beb8a0e5d9p15b205jsnb6b44eb3a127"
+		}
+	})
+	.then(response => {
+		return response.json()
+	})
+	.catch(err => {
+		console.error(err);
+	});
+};
+getUserRepos2()
