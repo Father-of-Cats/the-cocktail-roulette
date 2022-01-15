598e1c53348c42668d6d877a60338c887e28372a
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2022-01-14 19:07:42 -0600

    Add functionality to geolocate
    - initialise map on user location
    - find user location
    - handle location error if browser does not support or if user denies permission

--- assets/script/nearby-search.js
+++ assets/script/nearby-search.js
@@ -1,13 +1,61 @@
-// Initialise map
+// Map variables
 let pos;
 let map;
+let bounds;
+let infoWindow;
+let currentInfoWindow;
+let service;
+let infoPane;
 
 function initMap() {
-  // Set default location and initailise variables
-  pos = { lat: -33.857, lng: 151.213 };
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: pos,
-    zoom: 15
-  });
+  // Initialise variables and Geolocate user
+  bounds = new google.maps.LatLngBounds();
+	infoWindow = new google.maps.InfoWindow;
+	currentInfoWindow = infoWindow;
+
+	// Try HTML5 geolocation
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(position => {
+			pos = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			};
+			map = new google.maps.Map(document.getElementById('map'), {
+				center: pos,
+				zoom: 15
+			});
+			bounds.extend(pos);
+
+			infoWindow.setPosition(pos);
+			infoWindow.setContent('Location found.');
+			infoWindow.open(map);
+			map.setCenter(pos);
+
+		}, () => {
+			// Browser supports geolocation but user denies permission
+			handleLocationError(true, infoWindow);
+		});
+	} else {
+		// Browser does not support geolocation
+		handleLocationError(false, infoWindow);
+	}
 }
 
+// Handle Geolocation error
+function handleLocationError(browserHasGeolocation, infoWindow) {
+	// Default to most desolate place on Earth
+	pos = {lat: -37.116185, lng: -12.3540164};
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: pos,
+    zoom: 12
+  });
+
+	// Display an InfoWindow at map center
+	infoWindow.setPosition(pos);
+	infoWindow.setContent(browserHasGeolocation ?
+		'Geolocation permissions denied. Using default location.' :
+		'Error: Your browser does not support geolocation.');
+	infoWindow.open(map);
+	currentInfoWindow = infoWindow;
+
+}
